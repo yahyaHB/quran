3745605c1d3e4d0a26abce865cd81aa6bccc6aa4
yahya <yalbarrawoui256@gmail.com>
2018-07-05 20:30:35 +0300

    adding the home page;

--- src/client/src/containers/home/index.js
+++ src/client/src/containers/home/index.js
@@ -1,11 +1,23 @@
 import React ,{ Component } from 'react'
-
+import { Link } from 'react-router-dom';
 
 import './style.css'
 export default class Home extends Component {
 
 
   render(){
-    return (<div > Home Page</div>)
+    return (
+      <div className='home-page-container'>
+          <div className='home-content-holder'>
+            <div className='logo'>
+
+            </div>
+            <div className='home-options-holder'>
+              <div className='home-option'><Link className='remove-anchor-style' to='/read'> Tafsis Page </Link></div>
+              <div className='home-option'><Link className='remove-anchor-style' to='/tafsir'>Reading Page </Link> </div>
+            </div>
+          </div>
+      </div>
+    );
   }
 }
