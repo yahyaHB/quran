f10f358aec6f30f9b699be5843b845b26c2a40f8
Eslam Abed El Rahman <iah-93@hotmail.com>
2018-07-06 13:19:12 +0300

    adding the reading page container

--- src/client/src/containers/readingPage/index.js
+++ src/client/src/containers/readingPage/index.js
@@ -1,11 +1,42 @@
-import React ,{ Component } from 'react'
+import React , { Component } from 'react'
 
+import NavBar from '../../components/navBar'
+import Page from '../../components/page'
 
 import './style.css'
 export default class Home extends Component {
+  state = {
 
+    surah : {
+      name : 'test',
+      ayatNumber :0 ,
+      origin : 'makan el nozol'
+    },
+    currentPage : 1 ,
+    page : {}
 
+  }
+
+  goToSurah = (surahStartPage) => {
+
+  }
+
+  goToPage = (page) => {
+    fetch(`https://qurn.herokuapp.com/api/page/${page || 2 }`)
+    .then( res => res.json())
+    .then(page => this.setState({ page }))
+    .catch(err => alert(err.message))
+  }
+  
   render(){
-    return (<div > read Page</div>)
+    const { name , ayatNumber , origin , page }  = this.state.surah
+    const surah = { name , ayatNumber , origin }
+
+    return (
+      <div className='reading-page'>
+        <NavBar surah={surah} />
+        <Page page={page} />
+      </div>
+    )
   }
 }
